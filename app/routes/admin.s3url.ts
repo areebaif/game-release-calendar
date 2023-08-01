dc3b6bce005664fef454b426a3f6476a98b10ff6
areebaif <areeba.iftikhar1993@gmail.com>
2023-08-01 17:18:29 -0500

    roll back image upload on s3 if for any reason database does not save the corresponding game

--- app/routes/admin.s3url.ts
+++ app/routes/admin.s3url.ts
@@ -1,31 +1,8 @@
-import type { ActionArgs, TypedResponse, LoaderArgs } from "@remix-run/node";
-import { GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
+import type { ActionArgs } from "@remix-run/node";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { v4 as uuidv4 } from "uuid";
 import { s3Client, validFileType, s3FormFields } from "~/utils";
-
-// export async function loader({ params }: LoaderArgs) {
-//   // console.log(key, "holla");
-//   const fileType = params.fileType;
-//   // this will be only png
-//   console.log(
-//     fileType,
-//     "@@@@@@@@@@@@@@@@@@@@@@@@@@!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!dhdhjdjhdjh"
-//   );
-//   const file = validFileType(fileType);
-//   if (!file.isValid!) throw new Error("bad request: file type not valid");
-//   const fileName = `game/${uuidv4()}.${fileType}`;
-//   const s3Params = {
-//     Bucket: process.env.BUCKET_NAME,
-//     Key: fileName,
-//     ContentType: file.fileExtension!,
-//     // ACL: 'bucket-owner-full-control'
-//   };
-//   const command = new PutObjectCommand(s3Params);
-//   const signedUrl = await getSignedUrl(s3Client, command, { expiresIn: 60 });
-//   console.log(signedUrl, fileName);
-//   return { fileName, signedUrl };
-// }
 
 export const action = async ({ request }: ActionArgs) => {
   const form = await request.formData();
